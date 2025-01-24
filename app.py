72a91d646d480366f35c00fa18e1b1ecd95c13d3
Utkarsh-me <kulkarniutkarsh19@gmail.com>
2025-01-24 19:53:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import os
 import json
 
 # URL of the JSON file on GitHub (raw URL)
-FIREBASE_KEY = "https://raw.githubusercontent.com/Utkarsh-me/House_rent_prediction/refs/heads/main/house-rent-prediction-43bd8-firebase-adminsdk-fbsvc-bec842c544.json"
+FIREBASE_KEY = "https://github.com/Utkarsh-me/House_rent_prediction/blob/main/house-rent-prediction-43bd8-firebase-adminsdk-fbsvc-bec842c544.json"
 #FK = os.getenv("FIREBASE_KEY")
 #key_data = json.loads(FIREBASE_KEY)
 cred = credentials.Certificate(FIREBASE_KEY)
