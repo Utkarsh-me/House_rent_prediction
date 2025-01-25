aff2090ca6a378c5000eb5b19c809127c896e87e
Utkarsh-me <kulkarniutkarsh19@gmail.com>
2025-01-25 20:13:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,10 +20,10 @@ import json
 
 #___________________________________________
 
-#FIREBASE_KEY_PATH = os.getenv("FIREBASE_KEY")
-#if not FIREBASE_KEY_PATH:
-#    raise ValueError("FIREBASE_KEY_PATH environment variable not set")
-#cred = credentials.Certificate(FIREBASE_KEY_PATH)
+FIREBASE_KEY_PATH = os.getenv("FIREBASE_KEY")
+if not FIREBASE_KEY_PATH:
+    raise ValueError("FIREBASE_KEY_PATH environment variable not set")
+cred = credentials.Certificate(FIREBASE_KEY_PATH)
 
 #___________________________________________
 
